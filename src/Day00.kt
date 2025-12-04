c358977e1e4cf362f1b7814146c7c056671df052
snedbalek-paylocity <snedbalek@paylocity.com>
2025-12-04 09:57:18 +0100

    Initial commit

---
+++ src/Day00.kt
@@ -0,0 +1,22 @@
+import kotlin.time.measureTime
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+    val testInput = readInput("Day00_test")
+
+    val input = readInput("Day00")
+
+    val times = listOf(
+        measureTime { println(part1(testInput)) },
+        measureTime { println(part1(input)) },
+        measureTime { println(part2(testInput)) },
+        measureTime { println(part2(input)) },
+    )
+    println(times)
+}
